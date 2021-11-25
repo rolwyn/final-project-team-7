9dda27a9c0f3b0917ed54414a3ca0c3bd832c4f0
Rolwyn Ronald Quadras <quadras.r@northeastern.edu>
2021-11-25 01:48:30 -0500

    Added .env and config file, created atlas cluster

--- backend/api/app.js
+++ backend/api/app.js
@@ -2,10 +2,13 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import config from '../config/config.js'
+
+const host = config.DB_HOST
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/eventsdb');
+mongoose.connect(host, {useNewUrlParser: true});
 
 app.use(cors());
 app.use(express.json());
