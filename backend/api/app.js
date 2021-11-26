5bc661b3a3cee42833dde33bc75ecc4a9f53c655
Rolwyn Ronald Quadras <quadras.r@northeastern.edu>
2021-11-26 00:27:23 -0500

    Created model and routes for user authentication

--- backend/api/app.js
+++ backend/api/app.js
@@ -3,11 +3,14 @@ import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import config from '../config/config.js'
+import routes from './routes/index.js';
+// import models from './models/index.js';
 
 const host = config.DB_HOST
 
 const app = express();
 
+// use new parser else set it to false to use old url parser
 mongoose.connect(host, {useNewUrlParser: true});
 
 app.use(cors());
