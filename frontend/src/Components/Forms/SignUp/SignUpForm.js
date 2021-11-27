ba694a1a3c29d769554e576c27dbb5b1a36e028a
Kush Salaskar <salaskar.k@northeastern.edu>
2021-11-27 16:44:33 -0500

    Google login working

--- frontend/src/Components/Forms/SignUp/SignUpForm.js
+++ frontend/src/Components/Forms/SignUp/SignUpForm.js
@@ -43,9 +43,9 @@ function SignUpForm() {
                 <GoogleLogin
                     clientId="168488668480-hmla08j8tjo5dfq1571gfil3r0n36qig.apps.googleusercontent.com"
                     buttonText="Login with Google"
-                    onSuccess={handleSuccess()}
-                    onFailure={handleFailure()}
-                    cookiePolicy={'single_host_origin'}
+                    onSuccess={handleSuccess}
+                    onFailure={handleFailure}
+                    cookiePolicy='single_host_origin'
                 />
             </div>
         </form>      
