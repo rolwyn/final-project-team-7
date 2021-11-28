3b62e4054ca5bba5645e7a0205f5ae73364c59b1
aamrahms <aamrah.a@northeastern.edu>
2021-11-28 00:32:21 -0500

    Form added

--- frontend/src/Components/EventCreation/EventCreation.js
+++ frontend/src/Components/EventCreation/EventCreation.js
@@ -32,7 +32,7 @@ class EventCreation extends React.Component {
         <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="white"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/></svg>
         return <div>
             <div id="form">
-                    <h1> Create an Event</h1>
+                    <h1 className="star"> Create an Event</h1>
                     <div className="formElement">
                         <label> Event Name</label>
                         <input type="text" name="eventName" id="eventName" onChange={(event)=>this.change(event, "eventName")} required/>
@@ -54,10 +54,9 @@ class EventCreation extends React.Component {
                 
                 <button className="closeAdd" onClick={this.closeAdd}>{close}</button>
             </div>
-            <input type="file" />
         </div>
     }
 }
  
 export default EventCreation;
-<input type="file" />
+
