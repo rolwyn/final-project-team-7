92ecf1b29c614bc58c7646979016800050c3cc7d
Kush Salaskar <salaskar.k@northeastern.edu>
2021-11-29 18:04:46 -0500

    Modified home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
 import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import SignUp from './Views/SignUp/SignUp';
 import './App.scss';
 
 function App() {
+    const [isSignup, setIsSignup] = useState(false)
+
     return(
         <div>
+            <nav>
+                hello
+                <button onClick={() => setIsSignup(!isSignup)}>Login</button>
+            </nav>
             <Router>
                 <Routes>
-                    <Route exact path="/" element={<SignUp/>}/>
+                    {
+                        isSignup ? 
+                        <Route exact path="/" element={<SignUp/>}/> : 
+                        <Route exact path="/" element={<h1>Hello Cards</h1>}/>
+                    }
                 </Routes>
             </Router>
         </div>
