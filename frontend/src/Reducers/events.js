381df0151a11b7ff70469c79a35201fd23cab3c8
Kush Salaskar <salaskar.k@northeastern.edu>
2021-12-02 18:59:07 -0500

    Fetching posts through redux

--- frontend/src/Reducers/events.js
+++ frontend/src/Reducers/events.js
@@ -3,7 +3,7 @@ import { FETCH_ALL, CREATE } from "../Constants/actionTypes"
 const eventsReducer = (events = [], action) => {
     switch(action.type){
         case FETCH_ALL:
-            return events
+            return action.payload
         case CREATE:
             return events
         
