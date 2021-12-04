1f3feff940ec1436c6c6cac28fbed4fa2ffd95f1
Kush Salaskar <salaskar.k@northeastern.edu>
2021-12-04 00:11:06 -0500

    Added buttons

--- frontend/src/Components/EventCard/EventCard.js
+++ frontend/src/Components/EventCard/EventCard.js
@@ -3,7 +3,22 @@ import './EventCard.scss';
 
 
 function EventCard(props){
-    console.log(props.event)
+   
+    const handleLike = (e) => {
+        e.preventDefault()
+        console.log("Like clicked")
+    }
+
+    const handleEdit = (e) => {
+        e.preventDefault()
+        console.log("Edit clicked")
+    }
+
+    const handleDelete = (e) => {
+        e.preventDefault()
+        console.log("Delete clicked")
+    }
+
     return (
         <div className="card">
             {/* {flagIcon} */}
@@ -11,10 +26,9 @@ function EventCard(props){
             by {props.event.name}
             <img className="eventImg" src={props.event.img} alt="event-pic"/>
             <article>{props.event.description}</article>
-            {/* <div className="options">
-                {this.props.heartIcon}
-                {this.props.scheduleIcon}
-            </div>   */}
+            <button onClick={handleLike}>Like</button>
+            <button onClick={handleEdit}>Edit</button>
+            <button onClick={handleDelete}>Delete</button>
         </div>
     )
 }
