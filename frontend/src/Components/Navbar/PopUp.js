16f9d474f511c74dfd7cc2e395c1043da8f61528
aamrahms <aamrah.a@northeastern.edu>
2021-12-06 02:48:51 -0500

    Pop up changes

--- frontend/src/Components/Navbar/PopUp.js
+++ frontend/src/Components/Navbar/PopUp.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
 import EventCreation  from '../EventCreation/EventCreation';
+import styled from 'styled-components'
+import { MdClose } from 'react-icons/md';
 import './PopUp.scss';
 
 export default function PopUp(){
@@ -8,14 +10,20 @@ export default function PopUp(){
     const openModal=()=>{
         setShowModal(prevModal=>!prevModal);
     }
+    
+
     return (
         <>
-        <div>
+        {/* <div> */}
             {/* <ModelWrapper showModal={showModal}> */}
             <button onClick={openModal}>Add</button>
-            <EventCreation showModal={showModal} setShowModal={setShowModal}></EventCreation>
-            {/* </ModelWrapper> */}
-        </div>
+            {/* <div className="Background">
+                <div className="ModalWrapper"> */}
+                    <EventCreation showModal={showModal} setShowModal={setShowModal}></EventCreation>
+                    {/* <div className="CloseModalButton" onClick={()=>setShowModal(prev=>!prev)}></div> */}
+                {/* </div>
+            </div> */}
+        {/* </div> */}
         </>
     );
 }
