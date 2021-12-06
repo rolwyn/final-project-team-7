d3c656630cf399ed4d978241d643c2c676c0fb38
Kush Salaskar <salaskar.k@northeastern.edu>
2021-12-06 17:57:28 -0500

    Added google calander functionality

--- frontend/src/Components/EventCard/EventCard.js
+++ frontend/src/Components/EventCard/EventCard.js
@@ -11,7 +11,7 @@ function EventCard(props){
     const CLIENT_ID = process.env.REACT_APP_CLIENT_ID
     const API_KEY = process.env.REACT_APP_KUSH_CALENDER_API_KEY
     let DISCOVERY_DOCS =  ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"]
-    let SCOPES = "https://www.googleapis.com/auth/calender.events"
+    let SCOPES = "https://www.googleapis.com/auth/calendar"
     // const [isEventLiked, setIsEventLiked] = useState(isLiked(props, user))
 
     const Hearts = () => {
@@ -56,6 +56,45 @@ function EventCard(props){
             gapi.client.load('calendar', 'v3', () => console.log("loaded calendar"))
 
             gapi.auth2.getAuthInstance().signIn()
+            .then(() => {
+                let event = {
+                    'summary': 'Awesome Event!',
+                    'location': '800 Howard St., San Francisco, CA 94103',
+                    'description': 'Really great refreshments',
+                    'start': {
+                      'dateTime': '2020-06-28T09:00:00-07:00',
+                      'timeZone': 'America/Los_Angeles'
+                    },
+                    'end': {
+                      'dateTime': '2020-06-28T17:00:00-07:00',
+                      'timeZone': 'America/Los_Angeles'
+                    },
+                    'recurrence': [
+                      'RRULE:FREQ=DAILY;COUNT=2'
+                    ],
+                    'attendees': [
+                      {'email': 'lpage@example.com'},
+                      {'email': 'sbrin@example.com'}
+                    ],
+                    'reminders': {
+                      'useDefault': false,
+                      'overrides': [
+                        {'method': 'email', 'minutes': 24 * 60},
+                        {'method': 'popup', 'minutes': 10}
+                      ]
+                    }
+                }
+
+                let req = gapi.client.calendar.events.insert({
+                    'calendarId': 'primary',
+                    'resource': event,
+                })
+
+                req.execute(event => {
+                    console.log(event)
+                    window.open(event.htmlLink)
+                })
+            })
         })
         
     }
