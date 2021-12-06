d2adb4c65fb27572f30069ae74ad0fea31ef646f
Rolwyn Ronald Quadras <quadras.r@northeastern.edu>
2021-12-06 15:04:45 -0500

    Changed navbar styles

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -5,14 +5,14 @@ import Avatar from 'react-avatar';
 import './Navbar.scss';
 
 const Navbar = ({ user, setUser, isSignup, setIsSignup }) => {
-  const location = useLocation()
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
+    const location = useLocation()
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
 
-  useEffect(() => {
+    useEffect(() => {
     const token = user?.token
     setUser(JSON.parse(localStorage.getItem('userProfile')))
-  }, [location])
+    }, [location])
 
   //need to add logic for create
   // const addEvent = () => {
@@ -25,18 +25,18 @@ const Navbar = ({ user, setUser, isSignup, setIsSignup }) => {
   //     console.log(issignin)
   //     console.log("reached here")
 
-  const isLoggedIn = useSelector((state) => state.profile)
+    const isLoggedIn = useSelector((state) => state.profile)
 
-  const logout = () => {
-    dispatch({ type: "LOGOUT" })
-    dispatch({ type: "ISNOTSIGNIN" })
-    navigate('/')
-  }
-  //need to add logic for create
-  const addEvent = () => {
-    dispatch({ type: "" })
-    navigate('/')
-  }
+    const logout = () => {
+        dispatch({ type: "LOGOUT" })
+        dispatch({ type: "ISNOTSIGNIN" })
+        navigate('/')
+    }
+    //need to add logic for create
+    const addEvent = () => {
+        dispatch({ type: "" })
+        navigate('/')
+    }
 
   //const profileImg = user.imageurl;
   //console.log(user.profileObj, typeof user.profileObj);
@@ -46,33 +46,34 @@ const Navbar = ({ user, setUser, isSignup, setIsSignup }) => {
 
 
   return (
-    <nav>
-      <div>
-        <ul className='container'>
-          <li className='logo'>Events Tracker</li>
-
-
-          {user?.profileObj === undefined ?
-            <li>
-              <button className='loginbtn' onClick={() => setIsSignup(!isSignup)}>
-                <a href="/auth">Login</a>
-              </button>
-            </li> :
-            <div>
-              <li> <button>
-                <Avatar className='rounded-full flex items-center flex-shrink-0 profilebtn' size="50" round={true} name={user.profileObj.givenName + " " + user.profileObj.familyName} maxInitials={2} type="button" onClick={() => dispatch({ type: "ISSIGNIN" })}
-                />
-              </button></li>
-              <li><button className='addbtn' onClick={addEvent}>Add</button></li>
-              <li> <button className='addbtn' onClick={() => dispatch({ type: "ISNOTSIGNIN" })}> Home </button> </li>
-              <li><button className='loginbtn' onClick={logout}>Logout
-              </button>
-              </li>
+    <header>
+        <nav class="navbar_nav">
+            <div className="brand_logo" onClick={() => dispatch({ type: "ISNOTSIGNIN" })}>
+                <div className='logo'><img alt="brandlogo" src="assets/images/logo.png"></img></div>
             </div>
-          }
-        </ul>
-      </div>
-    </nav>
+            <ul className="nav_container">
+                {user?.profileObj === undefined ?
+                    <li>
+                    <button className='loginbtn' onClick={() => setIsSignup(!isSignup)}>
+                        <a href="/auth">Login</a>
+                    </button>
+                    </li> 
+                    :
+                    <>
+                    <li> 
+                        <button>
+                            <Avatar src={user?.profileObj.imageUrl} type="image" className='rounded-full flex items-center flex-shrink-0 profilebtn' size="50" round={true} onClick={() => dispatch({ type: "ISSIGNIN" })}
+                            />
+                        </button>
+                    </li>
+                    <li><button className='addbtn' onClick={addEvent}>Add</button></li>
+                    <li><button className='addbtn'> Home </button> </li>
+                    <li><button className='loginbtn' onClick={logout}>Logout</button></li>
+                    </>
+                }
+            </ul>
+        </nav>
+    </header>
   )
 }
 
