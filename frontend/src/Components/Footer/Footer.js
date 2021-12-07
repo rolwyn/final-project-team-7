461773ab8fa45260bbb0dce852c6877d134d9321
Devika Mujgule <mujgule.d@northeastern.edu>
2021-12-07 14:06:28 -0500

    correcting footer style

--- frontend/src/Components/Footer/Footer.js
+++ frontend/src/Components/Footer/Footer.js
@@ -4,16 +4,16 @@ export default function Footer(){
     return(
         <footer>
             <div className="wrapper"><a className="logo" href="//www.northeastern.edu"></a> 
-                <ul className="info">
-                    <li><a href="/" >My Profile</a></li>
+                {/* <ul className="info">
+                    <li><a href="/" >Event tracker</a></li>
                     {/* <li><a href="http://www.northeastern.edu/findfacultystaff" >Find Faculty &amp; Staff</a></li>
                     <li><a href="http://www.northeastern.edu/neuhome/adminlinks/findaz.html" >Find A-Z</a></li>
                     <li><a href="http://www.northeastern.edu/emergency/index.html" >Emergency Information</a></li>
                     <li><a href="http://assistive.usablenet.com/tt/http://www.northeastern.edu/index.html" >Text Only</a></li> 
-                    <li><a href="http://www.northeastern.edu/privacy/index.html" >Privacy</a></li>*/}
-                </ul>
+                    <li><a href="http://www.northeastern.edu/privacy/index.html" >Privacy</a></li>
+                </ul> */}
                 <div className="copyright">
-                    <p>&#169; <span>2017</span> Northeastern University </p>
+                    <p>&#169; <span>2017</span> Northeastern University <a href="/" >Event tracker</a> </p> 
                     <p>360 Huntington Ave., Boston, Massachusetts 02115 &#183; 617.373.2000 &#183; TTY 617.373.3768 </p>
                 </div>
             </div>
