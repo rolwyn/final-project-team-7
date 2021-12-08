684284c125dee137c3cb428a1cc36361cfa2a9a4
Devika Mujgule <mujgule.d@northeastern.edu>
2021-12-08 02:17:55 -0500

    Adding form validation for create, fixing styling

--- frontend/src/Components/EventCreation/test.js
+++ frontend/src/Components/EventCreation/test.js
@@ -212,3 +212,42 @@ let value = oneElement.target.value;
 
 export default EventCreation;
 
+
+
+//
+import React from 'react';
+import { useSelector } from 'react-redux';
+import EventCreation  from '../EventCreation/EventCreation';
+import './Modal.scss'
+
+export default function Modal({openModal, event, setShowModal} ){
+    const isAddModal = useSelector((state) => state.modal)
+    console.log(event)
+    return (
+
+        <div className="modal_fade">
+
+            <div className='popup'>
+                <div className="modal-body">
+                    test text remove later
+                    
+                    {/* {console.log("EditModal true") : */}
+                    {isAddModal?<EventCreation></EventCreation>:
+                        <h1>Edit modal</h1>
+                        
+                    }
+                    
+                </div>
+                {/* onClick={setShowModal(false)} */}
+                <div className="modal-close" >
+                    <img src="assets/images/close.png"/>
+                </div>
+=======
+       
+         </div>
+            
+          
+      </div>
+      
+    );
+}
