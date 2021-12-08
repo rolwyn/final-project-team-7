1cf32ed8f84be1a6c381054ac293a13ac2a976c1
Devika Mujgule <mujgule.d@northeastern.edu>
2021-12-07 20:57:46 -0500

    changing creation form

--- frontend/src/Components/EventCreation/test.js
+++ frontend/src/Components/EventCreation/test.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import Modal from 'react-modal';
 //import ReactDOM from 'react-dom';
 import Form from "react-validation/build/form";
-//import Input from "react-validation/build/input";
+import Input from "react-validation/build/input";
 import FileBase from 'react-file-base64';
 import { createEvent } from '../../Actions/events';
 import { useDispatch } from 'react-redux';
 import './EventCreation.scss';
+import CheckFieldsButton from "react-validation/build/button"
 
 const EventCreation = () => {
     const [eventName, setEventName] = useState("");
@@ -18,8 +19,11 @@ const EventCreation = () => {
     const [endTime, setEndTime] = useState("")
     const [location, setLocation] = useState("")
     const dispatch = useDispatch()
+    const [errorMsg, setErrorMsg] = useState("")
+    const formElement = React.useRef()
     //for modal
-    const user = JSON.parse(localStorage.getItem('userProfile'))
+    const user = JSON.parse(localStorage.getItem('userProfile')
+    )
 
     /**
      * Validation for creation of event form
@@ -29,7 +33,7 @@ const EventCreation = () => {
     const requiredField = (value) => {
         if (!value) {
             return (
-                <div className='text-red-500 text-sm italic mt-2'> Required Field!<sup>*</sup>
+                <div className='text-red-500 text-sm italic mt-2'><sup>*</sup> Required Field!
                 </div>
             )
         }
@@ -51,7 +55,9 @@ const EventCreation = () => {
 
     //whenever form is submitted
     const submitForm = async (e) => {
+        
         e.preventDefault();
+        formElement.current.validateAll()
         // checkIfNull;
         if (!user?.profileObj?.name) {
             clearAllFields();
@@ -66,25 +72,25 @@ const EventCreation = () => {
     }
     //whenever fields are updated
     const change = (oneElement, property) => {
-
+let value = oneElement.target.value;
         switch (property) {
             case 'setEventName':
-                setEventName(oneElement.target.value);
+                setEventName(value);
                 break;
             case 'setLocation':
-                setLocation(oneElement.target.value);
+                setLocation(value);
                 break;
             case 'setDescription':
-                setDescription(oneElement.target.value);
+                setDescription(value);
                 break;
             case 'setDate':
-                setDate(oneElement.target.value);
+                setDate(value);
                 break;
             case 'setTime':
-                setTime(oneElement.target.value);
+                setTime(value);
                 break;
             case 'setEndTime':
-                setEndTime(oneElement.target.value)
+                setEndTime(value)
                 break
             default: break;
         }
@@ -114,37 +120,78 @@ const EventCreation = () => {
     //render() { 
     // const close = <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="white"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/></svg>
     return (
-    <div className='container bg-white h-3/4'>
-        <Form onSubmit={(event) => submitForm(event)} className="form">
-            <div className='content_wrapper  px-5 py-10'>
-                
+        <div className='container bg-white h-3/4'>
+            <Form onSubmit={(event) => submitForm(event)} ref={formElement} className="form">
+                <div className='content_wrapper col-span-1 px-5 py-10'>
+
                     <h1 className="tagline"> Create an Event</h1>
                     <fieldset className="column_fieldset">
                         <label> Event Name</label>
-                        <input type="text" name="eventName" value={eventName} className="eventName" onChange={(event) => change(event, "setEventName")} required />
+                        <Input 
+                        type="text" 
+                        name="eventName" 
+                        value={eventName} 
+                        className="_inputField" 
+                        data-state='setEventName'
+                        onChange={change}
+                        validations={[requiredField]} 
+                       
+                        />
                     </fieldset>
                     <fieldset className="column_fieldset">
                         <label> Location</label>
-                        <input type="text" name="location" value={location} id="location" onChange={(event) => change(event, "setLocation")} required />
+                        <Input 
+                        type="text" 
+                        name="location" 
+                        className='_inputField'
+                        value={location} 
+                        id="location" 
+                        data-state='setLocation'
+                        validations={[requiredField]} 
+                        onChange={change} />
                     </fieldset>
                     <fieldset className="column_fieldset">
                         <label> Description</label>
-                        <input type="textarea" value={description} name="desc" className="desc" onChange={(e) => change(e, "setDescription")} placeholder="What's the event about?" required />
+                        <Input 
+                        type="textarea" 
+                        value={description} 
+                        name="desc" 
+                        className="_inputField" 
+                        data-state='setDescription'
+                        validations={[requiredField]} 
+                        onChange={change} 
+                        placeholder="What's the event about?"  />
                     </fieldset>
                     <div className=''>
-                    <fieldset className="column_fieldset col-span-1">
-                        <label> Date </label>
-                        <input type="date" value={date} name="date" className="desc" onChange={(e) => change(e, "setDate")} required />
-                    </fieldset>
-                    <fieldset className="column_fieldset col-span-2">
-                        <label> Time</label>
-                        <input type="time" value={time} name="time" className="desc" onChange={(e) => change(e, "setTime")} />
-                    </fieldset>
-                    </div>     
-                    <fieldset className="column_fieldset">
+                        <fieldset className="column_fieldset col-span-1">
+                            <label> Date </label>
+                            <Input 
+                            type="date" 
+                            value={date} 
+                            name="date" 
+                            className="_inputField" 
+                            data-state='setDate'
+                            validations={[requiredField]} 
+                            onChange={change}
+                        />
+                        </fieldset>
+                        <fieldset className="column_fieldset col-span-2">
+                            <label> Time</label>
+                            <Input 
+                            type="time" 
+                            value={time} 
+                            name="time" 
+                            className="_inputField" 
+                            data-state='setTime'
+                            validations={[requiredField]} 
+                            onChange={change}
+                             />
+                        </fieldset>
+                    </div>
+                    {/* <fieldset className="column_fieldset">
                         <label>End Time</label>
                         <input type="time" value={endTime} name="endTime" className="desc" onChange={(e) => change(e, "setEndTime")} required />
-                    </fieldset>
+                    </fieldset> */}
                     <fieldset className="column_fieldset right">
                         <label> Image </label>
                         {/* <Input type="file"  accept="image/*" name="image" id="file" /> */}
@@ -152,13 +199,13 @@ const EventCreation = () => {
                         <FileBase type="file" multiple={false} onDone={(base64) => onFileUpload(base64)} />
                     </fieldset>
                     <button onSubmit={(event) => submitForm(event)} className="save" type="submit">Add</button>
-               
-            </div>
 
-            {/* <button className="closeAdd" onClick={()=>closeAdd}>{close}</button> */}
-        </Form>
+                </div>
 
-    </div>
+                {/* <button className="closeAdd" onClick={()=>closeAdd}>{close}</button> */}
+            </Form>
+
+        </div>
     )
 }
 
